931d978202c90a7c6a0cdb864a3730fa55eaa149
Tran Quoc Huy <tran09112005@gmail.com>
2025-04-03 17:23:49 +0700

    update để tìm được dịch vụ phù hợp

--- src/main/java/com/example/skincare/repositories/ServiceRepository.java
+++ src/main/java/com/example/skincare/repositories/ServiceRepository.java
@@ -3,5 +3,8 @@ package com.example.skincare.repositories;
 import com.example.skincare.models.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ServiceRepository extends JpaRepository<Service, Long> {
+    Optional<Service> findByName(String name);
 }
